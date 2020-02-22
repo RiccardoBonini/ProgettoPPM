54aee9351495629cdcdb33262b838d21461aa1df
RiccardoBonini <38753018+RiccardoBonini@users.noreply.github.com>
2020-02-22 18:11:59 +0100

    Add files via upload

--- Configurazioni.js
+++ Configurazioni.js
@@ -5,19 +5,42 @@ function connect(){
     this.MySocket = new WebSocket('ws://192.168.43.209:256')
 }
 
-function send1() {
+function driveMode() {
     //const socket = new WebSocket('ws://192.168.43.201:255', ["protocolOne", "protocolTwo"]);
     var msg = {
-        type: "message",
-        text: "aooo",
+        dir1: "avanti",
+        dir2: "indietro",
+        dir3: "ruotaDestra",
+        dir4: "ruotaSinistra"
     };
     if(MySocket.readyState == 1) {
         MySocket.send(JSON.stringify(msg));
-
     }
 
 }
 
-function send2(){}
+function cookingMode(){
+    var msg = {
+        dir1: "avanti",
+        dir2: "indietro",
+        dir3: "destra",
+        dir4: "sinistra"
+    };
+    if(MySocket.readyState == 1) {
+        MySocket.send(JSON.stringify(msg));
+    }
 
-function send3(){}
+}
+
+function reverseDriveMode(){
+    var msg = {
+        dir1: "indietro",
+        dir2: "avanti",
+        dir3: "ruotaSinistra",
+        dir4: "ruotaDestra"
+    };
+    if(MySocket.readyState == 1) {
+        MySocket.send(JSON.stringify(msg));
+    }
+
+}
