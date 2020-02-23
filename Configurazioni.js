145638320de6d4ccd35c0933c32f936d5dced6e4
RiccardoBonini <38753018+RiccardoBonini@users.noreply.github.com>
2020-02-23 21:10:22 +0100

    Add files via upload

--- Configurazioni.js
+++ Configurazioni.js
@@ -1,17 +1,42 @@
-const MySocket = new WebSocket('ws://192.168.43.209:256');//alternative socket 'ws://172.18.82.53:255'
+var port=256;
+var IP= 'ws://192.168.43.209:';
+var url = IP + port.toString();
+const MySocket = new WebSocket(url);//alternative socket 'ws://172.18.82.53:255'
 
 
-function connect(){
-    this.MySocket = new WebSocket('ws://192.168.43.209:256')
-}
 
-function driveMode() {
+
+function driveModeCar() {
     //const socket = new WebSocket('ws://192.168.43.201:255', ["protocolOne", "protocolTwo"]);
+    //const MySocket = new WebSocket('ws://192.168.43.209:256');//+ port.toString());
     var msg = {
-        dir1: "avanti",
-        dir2: "indietro",
-        dir3: "ruotaDestra",
-        dir4: "ruotaSinistra"
+        M:"1"
+    };
+    if(MySocket.readyState == 1) {
+
+        MySocket.send(JSON.stringify(msg));
+        port ++;
+    }
+
+}
+
+function driveModeSteam() {
+    //const socket = new WebSocket('ws://192.168.43.201:255', ["protocolOne", "protocolTwo"]);
+    //const MySocket = new WebSocket('ws://192.168.43.209:256');//+ port.toString());
+    var msg = {
+        M:"2"
+    };
+    if(MySocket.readyState == 1) {
+
+        MySocket.send(JSON.stringify(msg));
+        port ++;
+    }
+
+}
+
+function cookingModeCar(){
+    var msg = {
+        M: "3"
     };
     if(MySocket.readyState == 1) {
         MySocket.send(JSON.stringify(msg));
@@ -19,12 +44,9 @@ function driveMode() {
 
 }
 
-function cookingMode(){
+function cookingModeSteam(){
     var msg = {
-        dir1: "avanti",
-        dir2: "indietro",
-        dir3: "destra",
-        dir4: "sinistra"
+        M: "4"
     };
     if(MySocket.readyState == 1) {
         MySocket.send(JSON.stringify(msg));
@@ -32,15 +54,36 @@ function cookingMode(){
 
 }
 
-function reverseDriveMode(){
+function reverseDriveModeCar(){
     var msg = {
-        dir1: "indietro",
-        dir2: "avanti",
-        dir3: "ruotaSinistra",
-        dir4: "ruotaDestra"
+        M: "5"
     };
     if(MySocket.readyState == 1) {
         MySocket.send(JSON.stringify(msg));
     }
 
+}
+
+function reverseDriveModeSteam(){
+    var msg = {
+        M: "6"
+    };
+    if(MySocket.readyState == 1) {
+        MySocket.send(JSON.stringify(msg));
+    }
+
+}
+
+function openMode(evt, Mode) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(Mode).style.display = "block";
+    evt.currentTarget.className += " active";
 }
