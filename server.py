f8aba4d0f5e1a4a06534744ed0d75e1bdbc972fb
gioFNK <gio.fnk@gmail.com>
2020-02-23 21:10:20 +0100

    Add files via upload

--- server.py
+++ server.py
@@ -1,22 +1,8 @@
-import selectors
-import test
-sel = selectors.DefaultSelector()
+import key_press
 import socket
-import time
+import websocket
 
-VK_TAB  = 0x09
-VK_MENU = 0x12
-VK_LBUTTON = 0x01
-VK_RBUTTON = 0x02
-W = 0x57
-A = 0x41
-S = 0x53
-D = 0x44
-VK_UP = 0x26
-VK_LEFT = 0x25
-VK_RIGHT = 0x27
-VK_SPACE = 0x20
-
+#parsing
 def Parse(Stringa, Carattere):
     s=""
     for i in range(len(Stringa)):
@@ -24,40 +10,71 @@ def Parse(Stringa, Carattere):
             i=i+3
             while((Stringa[i]=="'")or(Stringa[i]=="b")or(Stringa[i]==":")or(Stringa[i]=='"'))and i<=len(Stringa):
                 i=(i+1)%len(Stringa)
-            while (Stringa[i]!=",")and(Stringa[i]!="'")and(Stringa[i]!="b")and i<=len(Stringa)and(Stringa[i]!="{"):
+            while (Stringa[i]!=",")and(Stringa[i]!='"')and(Stringa[i]!="'")and(Stringa[i]!="b")and i<=len(Stringa)and(Stringa[i]!="{"):
                 s=s+Stringa[i]
                 i=(i+1)%len(Stringa)
             return s
     return -1
-def Trova(Stringa, Carattere):
-  Indice = 0
-  while Indice < len(Stringa):
-    if Stringa[Indice] == Carattere:
-      return Indice
-    Indice = Indice + 1
-  return -1
-def Check(modello):
-    for i in range(len(lista)):
-        if lista[i].model==modello:
+def Check_modello(modello):
+    for i in range(len(lista_connessi)):
+        if lista_connessi[i].model==modello:
             return i
     return -1
+def Check_comando(parametro, valore, mod): #legenda cooking
+    if(parametro=="X"):
+        if(valore>mod.valore1):
+            return "direzione1" #padella destra
+        if(valore<mod.valore2):
+            return "direzione2" #padella sinistra
+    if(parametro=="Y"):
+        if (valore < mod.valore3):
+            return "direzione3" #padella avanti
+        if (valore > mod.valore4):
+            return "direzione4" #padella indietro
+
+class mode:
+    def __init__(self, v1, v2, v3, v4, d1, d2, d3, d4):
+        self.valore1 = v1
+        self.valore2 = v2
+        self.valore3 = v3
+        self.valore4 = v4
+        self.d1 = d1
+        self.d2 = d2
+        self.d3 = d3
+        self.d4 = d4
+
+#modalità
+drive=mode("410","280","510","690", "a", "i", "s", "d")
+rdrive=mode("340","270","510","690", "i", "a", "d", "s")
+cook=mode("670","440","500","690", "d", "s", "a", "i")
+
+
+
 class connessioni:
     def __init__(self, addr, conn, mod):
         self.address=addr
         self.connection=conn
         self.model=mod
+
 host = '192.168.43.209'
 port = 255
-stringa=""
-lista=[]
-connected = 3 #numero di connessioni accettate
+port_interface = 256
+
+lista_connessi = [] #lista connessi
+connected = 2  #numero di connessioni accettate
+
 s=socket.socket()
 s.bind((host, port))
 s.listen()
+stringa=""
 
-waiter=0
-print("Partito")
-while waiter<connected:
+
+waiter=0 #iteratore
+piattaforma=""
+
+print("Server Partito")
+
+while waiter < connected:
     conn, addr = s.accept()
     s.setblocking(1)
 
@@ -65,59 +82,82 @@ while waiter<connected:
         modello="macchina"
     if '192.168.43.188' in addr:
         modello="controller"
-    if '192.168.43.201' in addr:
-        modello="interfaccia"
-    if ((len(lista) == 0)or(Check(modello))==-1):
+    if ((len(lista_connessi) == 0)or(Check_modello(modello))==-1):
         a=connessioni(addr,conn,modello)
-        lista.append(a)
+        lista_connessi.append(a)
         print('Connected by', a.address[0], 'Tipo', a.model )
         waiter = waiter + 1
-    indice = Check("controller")
-    controller = lista[indice].connection
-    indice = Check("macchina")
-    macchina = lista[indice].connection
-    indice = Check("interfaccia")
-    interfaccia = lista[indice].connection
-waiter=0
-# while True:
-#     for i in range(len(lista)):
-#         print(lista[i].connection)
-#     data=str(interfaccia.recv(20480))
-#     print(data)
-while True:
-    #da copiare in ogni funzione
-    macchina.send(str.encode("a"))
-    test.PressKey(VK_UP)
-    time.sleep(1)
-    test.ReleaseKey(VK_UP)
-    ###
+    indice = Check_modello("controller")
+    controller = lista_connessi[indice].connection
+    indice = Check_modello("macchina")
+    macchina = lista_connessi[indice].connection
 
-    data = str(controller.recv(20480))
-    stringa = stringa + data
-    found = Trova(stringa, "}")
-    if (found > -1):
-        split = stringa.split("}", 1)
-        buona=split[0]
-        stringa=split[1]
-        x=Parse(buona, "X")
-        print(x)
-        # if(x>"486"):
-        #     print("x avanti")
-        #      macchina.send(str.encode("a"))
-         # elif(x<"486"):
-            #     print("x indietro")
-            #     macchina.send(str.encode("i"))
-            # y=Parse(buona, "Y")
-            # if (y > "995"):
-            #     print("y avanti")
-            # elif (y < "993"):
-            #     print("y indietro")
-            # z=Parse(buona, "B")
-            # if(z>"1"):
-            #     print("z avanti")
-# lsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-# lsock.bind((host, port))
-# lsock.listen()
-# print('listening on', (host, port))
-# lsock.setblocking(False)
-# sel.register(lsock, selectors.EVENT_READ, data=None)
+while True:  #while generale
+    #configurazione
+
+    print("Modalita' configurazione interfaccia")
+    websocket.importConfig(host, port_interface)
+
+    if(Parse(websocket.config, "M")=="1"):
+        modalita=drive
+        piattaforma="macchina"
+        print("Modalità Drive- Car")
+    if(Parse(websocket.config, "M")=="2"):
+        modalita=drive
+        piattaforma="steam"
+        print("Modalità Drive- Steam")
+    if (Parse(websocket.config, "M") == "3"):
+        modalita = cook
+        piattaforma = "macchina"
+        print("Modalità Cooking- Car")
+    if (Parse(websocket.config, "M") == "4"):
+        modalita = cook
+        piattaforma = "steam"
+        print("Modalità Cooking- Steam")
+    if (Parse(websocket.config, "M") == "5"):
+        modalita = rdrive
+        piattaforma = "macchina"
+        print("Modalità Reverse Drive- Car")
+    if (Parse(websocket.config, "M") == "6"):
+        modalita = rdrive
+        piattaforma = "steam"
+        print("Modalità Reverse Drive- Steam")
+
+    port_interface+=1
+
+    while True: #while utilizzo
+        data = str(controller.recv(20480))
+        stringa = stringa + data
+        if ("}" in stringa):
+            split = stringa.split("}", 1)
+            buona=split[0]
+            stringa=split[1]
+            comandoX=Check_comando("X", Parse(buona, "X"), modalita)
+            comandoY=Check_comando("Y", Parse(buona, "Y"), modalita)
+            if("true" in buona):
+                print("Exiting modalita' di utilizzo")
+                break
+            if(comandoX=="direzione1"):
+                 print(modalita.d1)
+                 if(piattaforma=="steam"):
+                    key_press.press(modalita.d1)
+                 if(piattaforma=="macchina"):
+                    macchina.send(str.encode(modalita.d1))
+            if (comandoX == "direzione2"):
+                print(modalita.d2)
+                if (piattaforma == "steam"):
+                    key_press.press(modalita.d2)
+                if (piattaforma == "macchina"):
+                    macchina.send(str.encode(modalita.d2))
+            if (comandoY == "direzione3"):
+                print(modalita.d3)
+                if (piattaforma == "steam"):
+                    key_press.press(modalita.d3)
+                if (piattaforma == "macchina"):
+                    macchina.send(str.encode(modalita.d3))
+            if (comandoY == "direzione4"):
+                print(modalita.d4)
+                if (piattaforma == "steam"):
+                    key_press.press(modalita.d4)
+                if (piattaforma == "macchina"):
+                    macchina.send(str.encode(modalita.d4))
--- websocket.py
+++ websocket.py
@@ -1,40 +1,40 @@
 import asyncio
 import websockets
 
-config = None
-changed= False
+global config
 
 async def echo(websocket, path):
     global config
     async for message in websocket:
         await websocket.send(message)
-        #msg = await websocket.recv()
         config = message
-        #print(message)
-        if config == '{"type":"message","text":"aooo"}':
+        if config != '':
             asyncio.get_event_loop().stop()
-            
 
-
-asyncio.get_event_loop().run_until_complete(
-    #websockets.serve(echo, '192.168.43.201', 255))
-    websockets.serve(echo, '172.18.82.53', 255))
-asyncio.get_event_loop().run_forever()
-print(config)
-
-
-#asyncio.get_event_loop().run_until_complete(
-    # websockets.serve(echo, '192.168.43.201', 255))
-     #websockets.serve(echo, '172.18.82.53', 255))
-#count = 0
-#while count<30000000:
-    #count = count +1
-#asyncio.get_event_loop().close()
-#print(config)
-
-def importConfig():
-    asyncio.get_event_loop().run_until_complete(
-    #websockets.serve(echo, '192.168.43.201', 255))
-        websockets.serve(echo, '172.18.82.53', 255))
+def importConfig(address, port):
+    asyncio.get_event_loop().run_until_complete(websockets.serve(echo, address, port))
     asyncio.get_event_loop().run_forever()
-    return config
+
+
+
+
+
+
+
+
+
+
+#!/usr/bin/env python
+
+# import asyncio
+# import websockets
+#
+# async def hello():
+#     uri = "ws://192.168.43.201:255"
+#     async with websockets.connect(uri) as websocket:
+#         #await websocket.send("Hello world!")
+#         t=await websocket.recv()
+#     print(t)
+# asyncio.get_event_loop().run_until_complete(hello())
+
+
